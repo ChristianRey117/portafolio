df6e5b8d8462258a85911155f892268a670320c6
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-09-16 23:41:54 -0400

    fix(menu): use menu component for all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,5 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Home', url: '/home', icon: 'mail' },
-    { title: 'Web sites', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Unity', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Apps', url: '/folder/archived', icon: 'archive' },
-    { title: 'Contact', url: '/folder/spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    MenuComponent,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
