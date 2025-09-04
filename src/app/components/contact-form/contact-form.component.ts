3a6db7ceec33059da0ef16211524e6ca8d91bd1f
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-09-03 22:19:56 -0400

    feat(contact-form): create componente to contact clients

---
+++ src/app/components/contact-form/contact-form.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    CardModule,
+    InputTextModule,
+    FloatLabelModule,
+    ButtonModule,
+  ],
+})
+export class ContactFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
