316081e0b7ce782247b445f4a8f134ff494471ec
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2026-03-03 13:06:50 -0500

    refact(home): add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,34 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { HeaderComponent } from './components/header/header.component';
+import { definePreset } from '@primeng/themes';
+
+const theme = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#FFDEF2',
+      100: '#FFD4EE',
+      200: '#FFBAE5',
+      300: '#FFABDF',
+      400: '#0077b6',
+      500: '#0077b6',
+      600: '#0077b6',
+      700: '#520038',
+      800: '#660046',
+      900: '#4A1238',
+      950: '#3B1C2F',
+    },
+    secondary: {
+      50: '#FFDEF2',
+      100: '#FFD4EE',
+      200: '#FFBAE5',
+      300: '#FFABDF',
+      400: '#00b4d8',
+      500: '#00b4d8',
+      600: '#00b4d8',
+    },
+  },
+});
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,7 +55,7 @@ import { HeaderComponent } from './components/header/header.component';
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: theme,
       },
     }),
   ],
--- src/app/components/skills-section/skills-section.component.ts
+++ src/app/components/skills-section/skills-section.component.ts
@@ -11,18 +11,28 @@ import { CommonModule } from '@angular/common';
 export class SkillsSectionComponent {
   skills = {
     languages: [
-      { name: 'Spanish', level: 97 },
-      { name: 'English', level: 89 },
+      { name: 'Spanish', level: 100 },
+      { name: 'French', level: 80 },
+      { name: 'English', level: 80 },
     ],
     technical: [
       { name: 'Angular', level: 92 },
       { name: 'React / TypeScript', level: 90 },
+      { name: 'Node.js', level: 80 },
+      { name: 'Unity', level: 80 },
+      { name: 'Ionic', level: 80 },
+      { name: 'Cordova', level: 80 },
+      { name: 'Git', level: 90 },
     ],
     soft: [
       'Problem Solving',
       'Team Leadership',
       'Agile Methodologies',
       'Critical Thinking',
+      'Adaptability and Flexibility',
+      'Curiosity and Initiative',
+      'Teamwork',
+      'Ethics and Responsibility',
     ],
   };
 }
